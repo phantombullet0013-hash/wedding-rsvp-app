7efdab8c8b7457a9e02c24106f5026109af14866
AFIQSYAHIRAN24 <AFIQSYAHIRAN000@GMAIL.COM>
2026-03-15 16:42:27 +0800

    Update API URLs to backend Render deployment

--- frontend/src/AdminLogin.js
+++ frontend/src/AdminLogin.js
@@ -4,8 +4,7 @@ import {useState} from "react";
 import axios from "axios";
 
 // const API = "https://YOUR-RENDER-URL.onrender.com";
-// const API = "https://wedding-rsvp-app-0n5a.onrender.com";
-const API = "https://wedding-rsvp-app-1.onrender.com";
+const API = "https://wedding-rsvp-app-0n5a.onrender.com";
 
 function AdminLogin(){
 
